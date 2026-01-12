7f8c9d8401425b75bb4eae23b0c890752761e0a2
Nathan Young <Nathan Young>
2026-01-12 12:25:15 -0800

    First Main Commit

---
+++ build/CMakeFiles/pdf.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pdf"
+  "output/main.pdf"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/pdf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
